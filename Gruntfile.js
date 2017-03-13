a0c1d53c2e06571c7bf3ae271df594703405d4a0
VenkatTadipaneni <tadipaneni1993@gmail.com>
2017-03-13 10:30:19 -0400

    Added grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,10 @@
 module.exports = function (grunt) {
+    var reactScripts = grunt.file.readJSON('package.json');
     // CONFIGURE GRUNT
     grunt.initConfig({
-        // get the configuration info from package.json file
-        // this way we can use things like name and version (pkg.name)
-        pkg: grunt.file.readJSON('package.json'),
-
-        // all of our configuration goes here
         jshint: {
-            // define the files to lint
             files: ['Gruntfile.js', 'src/*.js'],
-            // configure JSHint
             options: {
-                // more options here if you want to override JSHint defaults
                 globals: {
                     jQuery: true,
                 },
@@ -21,9 +14,7 @@ module.exports = function (grunt) {
                 additionalSuffixes: ['.js']
             }
         },
-
         uglify: {
-            // uglify task configuration
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
@@ -31,22 +22,21 @@ module.exports = function (grunt) {
                 src: 'src/**.js',
                 dest: 'dist/<%= pkg.name %>.min.js'
             },
-
+        },
+        exec: {
+            run: 'npm start',
+            build: ' npm run build'
         }
-
     });
 
     // Load the plugin that provides the task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-jsxhint');
+    grunt.loadNpmTasks('grunt-exec');
 
-
-    // Default task(s).
-    grunt.registerTask('default', ['jshint', 'start']);
-    grunt.registerTask('start', '', function () {
-        grunt.log.writeln('package', grunt.file.readJSON('package.json').scripts.start);
-        //grunt.file.readJSON('package.json').scripts.start
-        //'react-scripts start'
-    });
+    //tasks.
+    grunt.registerTask('default', 'jshint', 'build', 'start');
+    grunt.registerTask('build', ['exec:build']);
+    grunt.registerTask('start', ['exec:run']);
 };
